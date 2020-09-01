257b2b5e5ccc1fe6c1fad8e352e0150bc4310bfe
mildsalmon <mildsalmon@gmail.com>
2020-09-01 13:59:46 +0900

    add perceptron

---
+++ perceptron.py
@@ -0,0 +1,28 @@
+from sklearn.linear_model import Perceptron
+
+# 샘플과 레이블이다
+X = [[0,0], [0,1], [1,0], [1,1]]
+y = [0, 0, 0, 1]
+
+# 퍼셉트론을 생성한다.
+# tol는 종료 조건이다.
+# random_state는 난수의 시드이다
+clf = Perceptron(tol=1e-3,random_state=0)
+
+# 학습을 수행한다
+clf.fit(X, y)
+
+# 테스트를 수행한다
+print(clf.predict(X))
+
+y = [0, 1, 1, 0]
+
+clf.fit(X, y)
+
+print(clf.predict(X))
+
+y = [0, 1, 1, 1]
+
+clf.fit(X, y)
+
+print(clf.predict(X))
