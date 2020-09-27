0ff6d2eef1e2c736b60fdb2dc08aa89708b2d376
mildsalmon <mildsalmon@gmail.com>
2020-09-28 08:43:23 +0900

    20.09.27-1_learn tensorboard

--- tensor_ex/3min_ex1.py
+++ tensor_ex/3min_ex1.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 hello = tf.constant("hello, Tensor")
-
+print(hello)
 a = tf.constant(10)
 b = tf.constant(32)
 c = tf.add(a, b)
-
+print(c)
 sess = tf.Session()
 
 print(sess.run(hello))
